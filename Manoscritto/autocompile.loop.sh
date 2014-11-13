209f8900a0f686cb33206cf95cb3f237c2b6161e
Tommaso Bianucci <churli@gmail.com>
2014-11-13 19:35:25 +0100

    Aggiunte un po di cose minori a Intro, Tools e Decompositions. Aggiunti un paio di comandini sega a Commands. Aggiunti i primi due teoremi (da finire dimostrazioni) alla TT-Decomposition.
    Inoltre aggiunta la modifica ad autocompile per avere evince sempre running.

--- Manoscritto/autocompile.loop.sh
+++ Manoscritto/autocompile.loop.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+evince Out/tesi.pdf &
+EPID="$!"
+
 OLD=`find . -name "*.tex" -exec cat '{}' \;`
 while true; do
+    ERUN=`ps aux | awk '{print $2}' | grep "^$EPID$"`
+    if [ ! "$ERUN" ]; then
+	evince Out/tesi.pdf &
+	EPID="$!"
+    fi
     CUR=`find . -name "*.tex" -exec cat '{}' \;`
     if [[ "$CUR" != "$OLD" ]]; then
 	if [[ $RANDOM -ge 6552 ]]; then
