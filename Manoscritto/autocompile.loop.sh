500c2e562338f6d607004472d3df3583f8139d81
Tommaso Bianucci <churli@gmail.com>
2017-05-05 11:47:15 +0200

    Reviewing the thesis for final version. Added some references. Reviewed the autocompile loop.

--- Manoscritto/autocompile.loop.sh
+++ Manoscritto/autocompile.loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ECHECK=`ps aux | grep "evince Out/tesi.pdf" | awk '{print $2}'`
+ECHECK=$(ps aux | grep "evince Out/tesi.pdf" | awk '{print $2}')
 if [ "$ECHECK" ]; then
     EPID="$ECHECK"
 else
@@ -8,22 +8,28 @@ else
     EPID="$!"
 fi
 
-OLD=`find . -name "*.tex" -exec cat '{}' \; | md5sum | grep -o "^[^ ]*"`
+OLD=$(find . -name "*.tex" -exec cat '{}' 2>/dev/null \; | md5sum | grep -o "^[^ ]*")
+OLDCITE=$(find . -name "*.tex" -exec cat '{}' 2>/dev/null \; | grep -o "\\\cite{[^}]*}" | md5sum | grep -o "^[^ ]*")
 while true; do
     ERUN=`ps aux | awk '{print $2}' | grep "^$EPID$"`
     if [ ! "$ERUN" ]; then
 	evince Out/tesi.pdf &
 	EPID="$!"
     fi
-    CUR=`find . -name "*.tex" -exec cat '{}' 2>/dev/null \; | md5sum | grep -o "^[^ ]*"`
+    CUR=$(find . -name "*.tex" -exec cat '{}' 2>/dev/null \; | md5sum | grep -o "^[^ ]*")
+    CITE=$(find . -name "*.tex" -exec cat '{}' 2>/dev/null \; | grep -o "\\\cite{[^}]*}" | md5sum | grep -o "^[^ ]*")
     if [[ "$CUR" != "$OLD" ]]; then
-	if [[ $RANDOM -ge 6552 ]]; then
-	    #succede con 4/5 di prob
+	if [[ "${CITE}" == "${OLDCITE}" ]]; then
+	    echo "Starting target COMPILE"
 	    make compileauto
+	    echo "Target COMPILE completed"
 	else
+	    echo "Starting target BIBCOMPILE"
 	    make bibcompileauto
+	    echo "Target BIBCOMPILE completed"
 	fi
 	OLD=$CUR
+	OLDCITE=${CITE}
 	date
     fi
     sleep 2s
